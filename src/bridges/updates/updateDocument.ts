86106e131b91869cc706de81878b6e0644ad4c15
Aurélien Grimaud <a.grimaud@involt.io>
2025-11-02 16:58:58 +0100

    feat: set document name using palette and theme details in updateDocument function

--- src/bridges/updates/updateDocument.ts
+++ src/bridges/updates/updateDocument.ts
@@ -7,6 +7,7 @@ import {
   ThemeConfiguration,
   ViewConfiguration,
 } from '@a_ng_d/utils-ui-color-palette'
+import setPaletteName from '../../utils/setPaletteName'
 import Sheet from '../../canvas/Sheet'
 import Palette from '../../canvas/Palette'
 
@@ -56,6 +57,13 @@ const updateDocument = async (view: ViewConfiguration) => {
       fillColor: currentTheme.paletteBackground,
     },
   ]
+  document.name = setPaletteName(
+    palette.base.name,
+    currentTheme.name,
+    palette.base.preset.name,
+    palette.base.colorSpace,
+    currentTheme.visionSimulationMode
+  )
 
   // Update
   document.setPluginData('view', view)
