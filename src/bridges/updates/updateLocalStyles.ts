2f79ed893bc142743cf8fb86768a15d6559e3a0c
Aurélien Grimaud <a.grimaud@involt.io>
2026-01-12 15:25:15 +0100

    chore: update theme name handling in createLocalStyles and updateLocalStyles functions

--- src/bridges/updates/updateLocalStyles.ts
+++ src/bridges/updates/updateLocalStyles.ts
@@ -56,14 +56,18 @@ const updateLocalStyles = async (id: string) => {
         )
         const path = [
           item.paletteName,
-          item.themeName === ''
-            ? tolgee.t('themes.defaultName')
-            : item.themeName,
+          ...(item.id.includes('00000000000')
+            ? []
+            : [
+                item.themeName === ''
+                  ? tolgee.t('themes.defaultName')
+                  : item.themeName,
+              ]),
           item.colorName === ''
             ? tolgee.t('colors.defaultName')
             : item.colorName,
         ]
-          .filter((item) => item !== '' && item !== 'None')
+          .filter((item) => item !== '')
           .join(' / ')
 
         if (styleMatch !== undefined) {
